0251dc01e865f1da249780a4374d36a3bb139ded
Mina Dong <minadong404@gmail.com>
2025-07-18 17:45:29 +0900

    chore: 백엔드 초기 프로젝트 설정 및 기본 파일 추가

---
+++ backend/src/main/java/com/sidediary/side_diary/SideDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.sidediary.side_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideDiaryApplication.class, args);
+	}
+
+}
